9078070a4be839134df84227687939bdb382e376
MinorMist <48128719+MinorMist@users.noreply.github.com>
2021-10-01 09:28:58 +0530

    Create minor.java
    Accept ma Jaba

---
+++ 5.Java/minor.java
@@ -0,0 +1,11 @@
+class *YOUR USERNAME* {
+    public static void main(String[] args) {
+
+    String name = "*YOUR NAME*";
+    int age = *YOUR AGE* ;
+    String username = "*YOUR USERNAME*";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
+// First programming lang <3, still lazy
